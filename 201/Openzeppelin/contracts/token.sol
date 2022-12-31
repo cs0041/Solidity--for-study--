50c681e847648941e25db6ec625a3ac836aa2ee7
cs0041 <pamzalove20@hotmail.com>
2022-12-31 15:45:22 +0700

    Update token.sol

--- 201/Openzeppelin/contracts/token.sol
+++ 201/Openzeppelin/contracts/token.sol
@@ -8,4 +8,13 @@ contract MyToken is ERC20Capped, Ownable{
         _mint(msg.sender,100);
 
     }
+
+      function burn(uint256 amount) public virtual {
+        _burn(_msgSender(), amount);
+    }
+
+     function burnFrom(address account, uint256 amount) public virtual {
+        _spendAllowance(account, _msgSender(), amount);
+        _burn(account, amount);
+    }
 }
