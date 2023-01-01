75564551c10cce8229228a7d577d2b7385e4136f
cs0041 <pamzalove20@hotmail.com>
2023-01-01 16:38:08 +0700

    Update dextest.js

--- 201/Project - Dex/test/dextest.js
+++ 201/Project - Dex/test/dextest.js
@@ -1,5 +1,3 @@
-//The first order ([0]) in the BUY order book should have the highest price
-
 const Dex = artifacts.require('Dex')
 const Link = artifacts.require('Link')
 const truffleAssert = require('truffle-assertions')
@@ -25,6 +23,9 @@ contract('Dex', (accounts) => {
       dex.createLimitOrder(1, web3.utils.fromUtf8('LINK'), 10, 1)
     )
     await link.approve(dex.address, 500)
+    await dex.addToken(web3.utils.fromUtf8('LINK'), link.address, {
+      from: accounts[0],
+    })
     await dex.deposit(10, web3.utils.fromUtf8('LINK'))
     await truffleAssert.passes(
       dex.createLimitOrder(1, web3.utils.fromUtf8('LINK'), 10, 1)
@@ -35,14 +36,19 @@ contract('Dex', (accounts) => {
     let dex = await Dex.deployed()
     let link = await Link.deployed()
     await link.approve(dex.address, 500)
+    await dex.depositEth({ value: 3000 })
     await dex.createLimitOrder(0, web3.utils.fromUtf8('LINK'), 1, 300)
     await dex.createLimitOrder(0, web3.utils.fromUtf8('LINK'), 1, 100)
     await dex.createLimitOrder(0, web3.utils.fromUtf8('LINK'), 1, 200)
 
     let orderbook = await dex.getOrderBook(web3.utils.fromUtf8('LINK'), 0)
+    assert(orderbook.length > 0)
+    console.log(orderbook)
     for (let i = 0; i < orderbook.length - 1; i++) {
-      const element = array[index]
-      assert(orderbook[i] >= orderbook[i + 1])
+      assert(
+        orderbook[i].price >= orderbook[i + 1].price,
+        'not right order in buy book'
+      )
     }
   })
   //The SELL order book should be ordered on price from lowest to highest starting at index 0
@@ -55,9 +61,13 @@ contract('Dex', (accounts) => {
     await dex.createLimitOrder(1, web3.utils.fromUtf8('LINK'), 1, 200)
 
     let orderbook = await dex.getOrderBook(web3.utils.fromUtf8('LINK'), 1)
+    assert(orderbook.length > 0)
+
     for (let i = 0; i < orderbook.length - 1; i++) {
-      const element = array[index]
-      assert(orderbook[i] <= orderbook[i + 1])
+      assert(
+        orderbook[i].price <= orderbook[i + 1].price,
+        'not right order in sell book'
+      )
     }
   })
 })
