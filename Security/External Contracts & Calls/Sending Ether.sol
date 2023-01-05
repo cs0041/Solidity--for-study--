d6a7426f1716903e061003fd27dc8b3f117d9ff7
cs0041 <pamzalove20@hotmail.com>
2023-01-05 17:16:19 +0700

    add detail

--- Security/External Contracts & Calls/Sending Ether.sol
+++ Security/External Contracts & Calls/Sending Ether.sol
@@ -3,19 +3,23 @@
 // Sending Ether safely
 
 
-// send -> payable(msg.sender).send(amount);
+// send (2300 gas, returns bool) -> payable(msg.sender).send(amount);
 // Safe against re-entrency
 // 2300 Gas Stipend
 
-// transfer ->  payable(msg.sender).transfer(amount);
+// transfer (2300 gas, throws error) ->  payable(msg.sender).transfer(amount);
 // Safe against re-entrency
 // 2300 Gas Stipend
 // Sams as send() but with a built in require()  Revert on failure
 // require(msg.sender.send(amount))
 
-// msg.sender.call{value: amount}("")
+// call (forward all gas or set gas, returns bool) -> msg.sender.call{value: amount}("")
 // Unlimited gas to fallback
 // can re-entrancy attack
+
+// Which method should you use?
+// call in combination with re-entrancy guard is the recommended method to use after December 2019.
+
 pragma solidity =0.8.17;
 
 contract SendEther  {  
