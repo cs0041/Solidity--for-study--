7fad5a58ec96fd402c73c4b5a5c6964df6371bbc
cs0041 <pamzalove20@hotmail.com>
2023-01-09 18:24:22 +0700

    deploy proxy

--- Security/Smart Contract Upgradeability - Full/migrations/1_deploy_proxy.js
+++ Security/Smart Contract Upgradeability - Full/migrations/1_deploy_proxy.js
@@ -1,4 +1,5 @@
 const Dogs = artifacts.require('Dogs')
+const DogsUpdated = artifacts.require('DogsUpdated')
 const Proxy = artifacts.require('Proxy')
 
 module.exports = async function (deployer, network, accounts) {
@@ -16,5 +17,23 @@ module.exports = async function (deployer, network, accounts) {
   var nrOfDogs = await proxyDog.getNumberOfDogs()
   console.log('Before update: ' + nrOfDogs.toNumber())
 
+  //Deploy new version of Dogs
+  const dogsUpdated = await DogsUpdated.new()
+  proxy.upgrade(dogsUpdated.address)
 
+  //Fool truffle once again. It now thinks proxyDog has all functions.
+  proxyDog = await DogsUpdated.at(proxy.address)
+  //Initialize proxy state.
+  proxyDog.initialize(accounts[0])
+
+  //Check so that storage remained
+  nrOfDogs = await proxyDog.getNumberOfDogs()
+  console.log('After update: ' + nrOfDogs.toNumber())
+
+  //Set the nr of dogs through the proxy with NEW FUNC CONTRACT
+  await proxyDog.setNumberOfDogs(30)
+
+  //Check so that setNumberOfDogs worked with new func contract.
+  nrOfDogs = await proxyDog.getNumberOfDogs()
+  console.log('After change: ' + nrOfDogs.toNumber())
 }
