c0d9c984ffdf439ea8b533794ecd19919ff828cf
cs0041 <pamzalove20@hotmail.com>
2023-03-04 22:35:13 +0700

    Create PriceConsumerV3.sol

---
+++ Chainlink/PriceFeeds/PriceConsumerV3.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract PriceConsumerV3 {
+    AggregatorV3Interface internal priceFeed;
+
+    /**
+     * Network: Goerli Testnet 
+     * Aggregator: BTC/USD
+     * Address: 0xA39434A63A52E749F02807ae27335515BA4b07F7
+     */
+    constructor() {
+        priceFeed = AggregatorV3Interface(
+            0xA39434A63A52E749F02807ae27335515BA4b07F7
+        );
+    }
+
+    /**
+     * Returns the latest price.
+     */
+    function getLatestPrice() public view returns (int) {
+        // prettier-ignore
+        (
+            /* uint80 roundID */,
+            int price,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = priceFeed.latestRoundData();
+        return price;
+    }
+}
